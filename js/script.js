e1569d7e09c46c4c9278418f965ed4d167c90059
Yoga Eka <yogaekat@gmail.com>
2019-04-07 16:18:09 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//event pada saat link di klik
+$('.page-scroll').on('click', function(e){
+
+	// ambil isi href
+	var tujuan = $(this).attr('href');
+
+	// tangkap elemen yg bersangkutan
+	var elemenTujuan = $(tujuan);
+
+	// pindahkan scroll
+	$('html,body').animate({
+		scrollTop: elemenTujuan.offset().top - 50
+	}, 1250);
+
+	e.preventDefault();
+
+});
